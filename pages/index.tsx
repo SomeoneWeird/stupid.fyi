ab3c43722dbd1553760c87e8f95eaaf1ff0add5c
Adam Brady <adam@boxxen.org>
2020-04-16 22:33:32 +1000

    make work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,42 @@
-import { NextPage } from 'next'
+import { NextPage, NextPageContext } from 'next'
+import Head from 'next/head'
 
-const Go: NextPage = () => {
+interface GoProps {
+  host: string
+}
+
+const Go: NextPage<GoProps> = ({ host }) => {
+  const parts = host.split('.')
+  parts.pop() // .fyi
+  parts.pop() // stupid
+
+  if (parts[0]) {
+    parts[0] = parts[0].substr(0, 1).toUpperCase() + parts[0].substr(1)
+  }
+
+  const msg = `${parts.join(' ')} stupid FYI`
+
   return (
-    <div>blah</div>
+    <>
+      <Head>
+        <title>{msg}</title>
+        <meta property="og:description" content={msg} />
+      </Head>
+      <span>
+        <h1 style={{ textAlign: 'center', marginLeft: 'auto', marginRight: 'auto' }}>
+          {msg}
+        </h1>
+      </span>
+    </>
   )
+}
+
+export async function getServerSideProps (context: NextPageContext) {
+  return {
+    props: {
+      host: context.req.headers.host
+    }
+  }
 }
 
 export default Go
